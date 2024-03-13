0d97ecbb28a0231b7cdbf0bd7fbde1c64abbcd91
Johnndelembi <159067569+Johnndelembi@users.noreply.github.com>
2024-03-13 20:16:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -127,9 +127,9 @@ st.markdown('''
 
 st.write("###")
 
-st.subheader(" :orange[VOLUNTEER EXPERIENCE] ")
+st.subheader(" :blue[VOLUNTEER EXPERIENCE] ")
 
-st.write(" :orange[PSSSF], *Geita*")
+st.write(" :blue[PSSSF], *Geita*")
 st.write("July 2023 - September 2023")
 st.markdown('''
     - Participated in PSSSF volunteer training of the institution
@@ -137,14 +137,14 @@ st.markdown('''
     - Worked with Excell spreadsheets of members perfoming data wrangling and missing values treatment ''')
 
 st.write("###")
-st.write(" :orange[ENNOVATE VENTURES], Dar es Salaam")
+st.write(" :blue[ENNOVATE VENTURES], Dar es Salaam")
 st.write("April 2023 - June 2023")
 st.markdown('''
     - Worked as a developer tasked to design a web app with the aim to solve a problem in the community
     - Completed the task by 90% and submitted the project on time''')
 
-st.subheader(" :red[WORK EXPERIENCE] ")
-st.write(" :red[EVERYDAY MANIFESTATIONS],  *Dar es Salaam — C.E.O Personal Assistant*")
+st.subheader(" :blue[WORK EXPERIENCE] ")
+st.write(" :blue[EVERYDAY MANIFESTATIONS],  *Dar es Salaam — C.E.O Personal Assistant*")
 st.markdown("April 2022 - July 2022")
 st.markdown('''
     - Worked with the C.E.O in launching multiple marketing campaigns. 
@@ -152,7 +152,7 @@ st.markdown('''
 
 st.write("###")    
 
-st.write(" :red[BRENDALICIOUS FRESH JUICE],  *Mbeya — Business analyst, Graphics designer.*")
+st.write(" :blue[BRENDALICIOUS FRESH JUICE],  *Mbeya — Business analyst, Graphics designer.*")
 st.markdown("December 2022 - January 2023")
 st.markdown('''
     - Helped in re-establishing the the company into the business world by making better decisions in marketing and sales department which brought about the growth of the company by 75% from before i started working with.
@@ -161,7 +161,7 @@ st.markdown('''
 
 st.write("###")
 
-st.write(" :red[AIESEC IN UDSM], *Dar es Salaam -- Team member of Business and Patners development department*")
+st.write(" :blue[AIESEC IN UDSM], *Dar es Salaam -- Team member of Business and Patners development department*")
 st.markdown("June 2023 - Present")
 st.markdown('''
     - Participated in organizing AIESEC International Relations event with EwA members
